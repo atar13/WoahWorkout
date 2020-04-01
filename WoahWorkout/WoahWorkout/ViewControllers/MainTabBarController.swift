6c32db12f049772008f13bfb11694b7f7fbf82d1
Anthony Tarbinian <atar137h@gmail.com>
2020-03-31 18:36:13 -0700

    First TabBarItem with config method

--- WoahWorkout/WoahWorkout/ViewControllers/MainTabBarController.swift
+++ WoahWorkout/WoahWorkout/ViewControllers/MainTabBarController.swift
@@ -10,22 +10,41 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
 
+    
+    let startTab = UITabBarItem()
+    
+    override func loadView() {
+        super.loadView()
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+//        view.backgroundColor = .blue
+        
+        let startVC = StartViewController()
+        
+        setViewControllers([startVC], animated: true)
+        
+//        startTab.title = "Start"
+//        let startTabImage = UIImage(named: "StartTab")
+//        startTab.image = startTabImage
+        
+        configTabBarProperties(viewController: startVC, tabBarTitle: "Start", tabBarImageName: "StartTab")
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    /**
+            Method to configure tab bar item title and image properties
+            To configure the image set the parameter to the String value equivalent to the one in the Assets folder
+     */
+    func configTabBarProperties(viewController: UIViewController, tabBarTitle: String, tabBarImageName: String){
+        viewController.tabBarItem.title = tabBarTitle
+        let tabBarImage = UIImage(named: tabBarImageName)
+        viewController.tabBarItem.image = tabBarImage
     }
-    */
+
 
 }
